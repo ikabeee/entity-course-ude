a6dc73f76360e3526b2cc1d5b54c11bd74a841c0
Carlos Gonzalez <carlglz30@gmail.com>
2025-05-09 10:20:13 -0500

    Implement CRUD operations for tags in TagsController and remove unused user detail views

--- entity-course-ude/Controllers/TagsController.cs
+++ entity-course-ude/Controllers/TagsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using entity_course_ude.data;
+using entity_course_ude.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +13,49 @@ namespace entity_course_ude.Controllers
     [Route("[controller]")]
     public class TagsController : Controller
     {
+        public readonly ApplicationDBContext _context;
+        public TagsController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("Create")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Tag tag)
+        {
+            _context.Tag.Add(tag);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpGet("Edit")]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return View();
+            }
+            var tag = _context.Tag.FirstOrDefault(t => t.Tag_Id == id);
+            return View(tag);
+        }
+
+        [HttpPost("Edit")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Tag tag)
+        {
+            _context.Tag.Update(tag);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
